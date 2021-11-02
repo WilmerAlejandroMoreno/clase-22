b094cfc87bae8c0f1129eb8f96eff1d25ffcb546
WilmerAlejandroMoreno <Wilmeramv2810@gmail.com>
2021-11-02 18:01:13 -0500

    clase 22

---
+++ dom.js
@@ -0,0 +1,13 @@
+function recuperarEdad(){
+    var mensaje = ""
+var edad = document.getElementById("edad").value;
+if (edad < 18 ) {
+    mensaje="Menor de edad";
+}
+else {
+    mensaje="Mayor de edad";
+}
+var contenedor = document.createElement("p");
+contenedor.innerText = mensaje;
+document.body.appendChild (contenedor);
+}
